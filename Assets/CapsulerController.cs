e896b84c2b273e33024928022ef9c8b6e28a4fc6
cx <chixuchixu@hotmail.com>
2017-07-01 14:50:35 +0800

    init project

---
+++ Assets/CapsulerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsulerController : MonoBehaviour {
+
+	public float speed = 10;
+	Transform transform;
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Time.deltaTime * speed, Time.deltaTime * speed, Time.deltaTime*speed);
+	}
+}
